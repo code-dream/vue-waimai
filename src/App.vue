a3d7757e2b32c7d2392b1576ec44fc1921b34337
wangyou <wangyou@yeah.com>
2018-12-14 23:34:56 +0800

    静态资源大体制作完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
-      hello
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
+<script>
+import Footer from './components/Footer/Footer.vue'
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style>
-
+@import './assets/css/reset.css';
+@import './assets/iconfont/iconfont.css';
+body,html{
+  height: 100%;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Msite from './views/Msite/Msite.vue'
+import Order from './views/Order/Order.vue'
+import Profile from './views/Profile/Profile.vue'
+import Search from './views/Search/Search.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {
+    path: '/msite',
+    component: Msite
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/',
+      redirect: '/Msite'
+    }
   ]
 })
